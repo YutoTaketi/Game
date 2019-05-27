26cf9801a6f4f738691bd2b7057e7f325b882cf6
Yuto Taketi <kbc18b15@stu.kawahara.ac.jp>
2019-05-27 09:47:41 +0900

    BeeBalletをエフェクトに変更

--- GameTemplate/Game/BeeBallet.cpp
+++ GameTemplate/Game/BeeBallet.cpp
@@ -10,18 +10,18 @@ BeeBallet::BeeBallet()
 
 BeeBallet::~BeeBallet()
 {
-	DeleteGO(m_skinModelRender);
-	//DeleteGO(effect);
+	//DeleteGO(m_skinModelRender);
+	DeleteGO(effect);
 }
 
 bool BeeBallet::Start()
 {
-	/*effect = NewGO<prefab::CEffect>(0);
+	effect = NewGO<prefab::CEffect>(0);
 	//エフェクトを再生
 	effect->Play(L"effect/Tama.efk");
-	m_scale = { 3.0, 3.0, 3.0 };*/
-	m_skinModelRender = NewGO<prefab::CSkinModelRender>(0);
-	m_skinModelRender->Init(L"modelData/BeeBallet.cmo");
+	m_scale = { 3.0, 3.0, 3.0 };
+	//m_skinModelRender = NewGO<prefab::CSkinModelRender>(0);
+	//m_skinModelRender->Init(L"modelData/BeeBallet.cmo");
 	
 	return true;
 }
@@ -39,9 +39,10 @@ void BeeBallet::Update()
 	
 	m_position += m_moveSpeed;
 	m_position.y -= 5.0;
-	//effect->SetPosition(m_position);
-	//effect->SetScale(m_scale);
-	m_skinModelRender->SetPosition(m_position);
+	effect->SetPosition(m_position);
+	effect->SetScale(m_scale);
+	effect->SetRotation(m_rotation);
+	//m_skinModelRender->SetPosition(m_position);
 	m_timer++;
 	if (m_timer == 120) {
 		DeleteGO(this);
--- GameTemplate/Game/BeeBallet.h
+++ GameTemplate/Game/BeeBallet.h
@@ -12,11 +12,17 @@ public:
 
 	int m_timer = 0;
 	Bee* bee;
-	//prefab::CEffect* effect = nullptr;
-	prefab::CSkinModelRender* m_skinModelRender;
+	prefab::CEffect* effect = nullptr;
+	CVector3 m_position = CVector3::Zero;
+	CVector3 m_scale = CVector3::One;
+	CVector3 m_moveSpeed = CVector3::Zero;
+	CQuaternion m_rotation = CQuaternion::Identity;
+
+	/*prefab::CSkinModelRender* m_skinModelRender;
 	CVector3 m_position = CVector3::Zero;
 	CVector3 m_moveSpeed = CVector3::Zero;
 	CVector3 m_scale = CVector3::One;
 	CQuaternion m_rotation = CQuaternion::Identity;
+	*/
 };
 
